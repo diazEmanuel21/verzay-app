34c284d874c77cabb0241393181ad866732a56b0
AlexMoreR <amoreanor.2024@gmail.com>
2025-03-09 21:34:05 -0500

    Fix: appNode.ts

--- types/appNode.ts
+++ types/appNode.ts
@@ -1,10 +1,10 @@
-import { Node } from "@xyflow/react";
+// import { Node } from "@xyflow/react";
 
-export interface AppNodeData {
-    type: TackType
-    [key: string]: any;
-}
+// export interface AppNodeData {
+//     type: TackType
+//     [key: string]: any;
+// }
 
-export interface AppNode extends Node {
-  data: AppNodeData;
-}
+// export interface AppNode extends Node {
+//   data: AppNodeData;
+// }
