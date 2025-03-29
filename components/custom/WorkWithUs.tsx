a35d745642dbe6eb0cfefe0872955cb76339b572
Manu Diaz <diazev20@gmail.com>
2025-03-28 21:16:40 -0500

    si se daña es por culpa de xander merge

--- components/custom/WorkWithUs.tsx
+++ components/custom/WorkWithUs.tsx
@@ -77,6 +77,7 @@ export const WorkWithUs = () => {
               </h3>
 
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                
                 {feature.description}
               </p>
             </CardContent>
