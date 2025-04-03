9b0b19fcd399c8d6392ba7e7c8122cf00189d85a
Manu Diaz <diazev20@gmail.com>
2025-04-03 16:43:39 -0500

    se elimina vista antigua decapreted

--- app/(root)/(protected)/admin/conexion/_components/MainConnection.tsx
+++ app/(root)/(protected)/admin/conexion/_components/MainConnection.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { agregarApi, editarApiKey, eliminarApiKey } from "@/actions/api-action";
-import ApiKeysTable from "@/components/shared/apikeystable";
 import { DialogApiKeyType } from "../connection-types";
 import { ApiKey, User } from "@prisma/client";
 import { getColumns, DataGrid, CreateDialog, EditDialog, DeleteDialog } from "./";
@@ -130,38 +129,6 @@ export const MainConnection = ({ searchParams, user, apiKeys }: Props) => {
                     apikey={currentApiKey}
                 />
             )}
-
-            {/* 
-            <div className="container">
-                <form action={agregarApi} className="flex flex-col gap-y-4">
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            name="url"
-                            placeholder="Ingresa la url de Evolution"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            name="key"
-                            placeholder="Ingresa la Apikey"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                    </div>
-                    <input
-                        type="hidden"
-                        name="userId"
-                        value={user.id}
-                    />
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        AGREGAR
-                    </button>
-                </form>
-            </div>
-
-            <ApiKeysTable /> */}
         </>
     )
 };
