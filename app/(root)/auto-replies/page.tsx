4571f590191248182914d035398ebcd29a0a05d4
AlexMoreR <amoreanor.2024@gmail.com>
2025-04-09 22:27:21 -0500

    dev: contruccion

--- app/(root)/auto-replies/page.tsx
+++ app/(root)/auto-replies/page.tsx
@@ -3,6 +3,8 @@ import { GetWorkFlowforUser } from '@/actions/getWorkFlowforUser-action';
 import { Workflow } from '@prisma/client';
 import { Suspense } from 'react';
 import { AutoRepliesContent, SkeletonAutoReplies } from './_components';
+import { Card, CardContent } from '@/components/ui/card';
+import { Sparkles } from 'lucide-react';
 
 function hasWorkflow(result: { data?: Workflow[] }): result is { data: Workflow[] } {
     return !!result.data;
@@ -19,7 +21,26 @@ const AutoRepliesPage = async () => {
     const resWorkflow = await GetWorkFlowforUser(user.id);
     const workflows = hasWorkflow(resWorkflow) ? resWorkflow.data : [];
 
-    if (user) return <h1>En construcción...</h1>
+    if (user) {
+        return (
+            <div className="flex justify-center">
+                <Card className="w-full max-w-xl shadow-xl">
+                    <CardContent className="flex flex-col items-center text-center p-8">
+                        <Sparkles className="w-10 h-10 text-purple-500 animate-pulse mb-4" />
+                        <h1 className="text-2xl font-bold mb-2">
+                            🚧 ¡Algo increíble está por llegar!
+                        </h1>
+                        <p className="text-muted-foreground text-sm mb-4">
+                            Estamos desarrollando una nueva funcionalidad de <b>respuesta rápida</b> con envío de archivos adjuntos, además de <b>seguimientos inteligentes</b> impulsados por inteligencia artificial que conservan el contexto    .
+                            <br /><br />
+                            Muy pronto tendrás acceso a herramientas que potenciaran tu negocio como nunca antes lo habia experimentado.
+                        </p>
+                        <span className="text-xs text-gray-400">Verzay a tu servicio. 2025 💜</span>
+                    </CardContent>
+                </Card>
+            </div>
+        );
+    }
 
     return (
         <Suspense fallback={<SkeletonAutoReplies />}>
