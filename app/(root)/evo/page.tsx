f4d1f6b90aa3f0eab0cb0eb6615cc3889c95041c
Manu Diaz <diazev20@gmail.com>
2025-06-03 13:56:23 -0500

    evo hidden

--- app/(root)/evo/page.tsx
+++ app/(root)/evo/page.tsx
@@ -1,8 +1,14 @@
 import IframeRenderer from "@/components/custom/IframeRenderer";
+import { currentUser } from "@/lib/auth";
 
 const MultiagentePage = async () => {
+    const user = await currentUser();
     const url = "https://conexion-1.verzay.co/manager/";
-    
+
+    if (!user || user?.role !== "admin") {
+        return <div>Lo sentimos este portal solo esta hecho para distruibudores.</div>;
+    };
+
     return <IframeRenderer url={url} />;
 }
 
