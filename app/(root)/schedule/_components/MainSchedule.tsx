612faf915bf9e4e60715e34a5f5f32dfe9c304b4
diazEmanuel21 <diazev20@gmail.com>
2025-09-11 15:29:27 -0500

    responsive

--- app/(root)/schedule/_components/MainSchedule.tsx
+++ app/(root)/schedule/_components/MainSchedule.tsx
@@ -1,19 +1,19 @@
 import {
     Card,
     CardContent,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
     Tabs,
     TabsContent,
     TabsList,
     TabsTrigger,
-} from "@/components/ui/tabs"
+} from "@/components/ui/tabs";
 
 import { MainReminders } from "../../reminders/_components";
 import { MainReminderInterface } from "@/schema/reminder";
 import ServiceManager from './services/ServiceManager';
-import { CustomCalendar } from "./dashboard"
-import { ShareScheduleLinkButton, UserAvailabilityForm } from "./availability"
+import { CustomCalendar } from "./dashboard";
+import { ShareScheduleLinkButton, UserAvailabilityForm } from "./availability";
 import { UpdateMeetingDuration } from "./settings";
 
 export const MainSchedule = ({ isCampaignPage, user, apiKey, reminders, leads, workflows, instancia, }: MainReminderInterface) => {
@@ -22,7 +22,7 @@ export const MainSchedule = ({ isCampaignPage, user, apiKey, reminders, leads, w
     return (
         <div className="flex w-full flex-col gap-6">
             <Tabs defaultValue="dashboard">
-                <TabsList>
+                <TabsList className="overflow-x-auto flex gap-4 whitespace-nowrap">
                     <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
                     <TabsTrigger value="availability">Disponibilidad</TabsTrigger>
                     <TabsTrigger value="services">Servicios</TabsTrigger>
@@ -30,29 +30,29 @@ export const MainSchedule = ({ isCampaignPage, user, apiKey, reminders, leads, w
                     <TabsTrigger value="settings">Ajustes</TabsTrigger>
                 </TabsList>
                 <TabsContent value="dashboard">
-                    <Card className="border-none pt-6">
+                    <Card className="border-none pt-4">
                         <CardContent>
                             <CustomCalendar user={user} />
                         </CardContent>
                     </Card>
                 </TabsContent>
                 <TabsContent value="availability">
-                    <Card className="border-none mt-2">
-                        <CardContent className="flex flex-col pt-6 gap-2">
+                    <Card className="border-none pt-4">
+                        <CardContent className="flex flex-col gap-2">
                             <UserAvailabilityForm userId={userId} />
                             <ShareScheduleLinkButton userId={userId} />
                         </CardContent>
                     </Card>
                 </TabsContent>
                 <TabsContent value="services">
-                    <Card className="border-none">
+                    <Card className="border-none pt-4">
                         <CardContent className="flex flex-col gap-2 ">
                             <ServiceManager userId={userId} />
                         </CardContent>
                     </Card>
                 </TabsContent>
                 <TabsContent value="reminders">
-                    <Card className="border-none">
+                    <Card className="border-none pt-4">
                         <CardContent className="flex flex-col gap-2 ">
                             <MainReminders
                                 isCampaignPage={isCampaignPage}
@@ -69,9 +69,13 @@ export const MainSchedule = ({ isCampaignPage, user, apiKey, reminders, leads, w
                     </Card>
                 </TabsContent>
                 <TabsContent value="settings">
-                    <UpdateMeetingDuration userId={user.id} />
+                    <Card className="border-none pt-4">
+                        <CardContent className="flex flex-col gap-2 ">
+                            <UpdateMeetingDuration userId={user.id} />
+                        </CardContent>
+                    </Card>
                 </TabsContent>
             </Tabs>
         </div>
-    )
+    );
 }
