44811e20dd28c2921ea8635fab7bf40191303710
diazEmanuel21 <diazev20@gmail.com>
2025-09-18 16:38:41 -0500

    fix

--- app/(root)/multiagente/page.tsx
+++ app/(root)/multiagente/page.tsx
@@ -1,9 +1,15 @@
-import IframeRenderer from "@/components/custom/IframeRenderer";
+"use client";
 
-const MultiagentePage = async () => {
+const MultiagentePage = () => {
     const url = "https://multiagente.ia-app.com";
-    
-    return <IframeRenderer url={url} />;
-}
+
+    return (
+        <iframe
+            src={url}
+            allow="microphone; autoplay; clipboard-read; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            allowFullScreen
+        />
+    );
+};
 
 export default MultiagentePage;
