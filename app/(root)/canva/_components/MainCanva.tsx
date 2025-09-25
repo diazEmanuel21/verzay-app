4e7cff5a9eeafa71385241dfb12a81f3d87bb0ea
Eros Hervilla <cupidreborn522@gmail.com>
2025-09-25 16:41:42 -0400

    corregido canva

--- app/(root)/canva/_components/MainCanva.tsx
+++ app/(root)/canva/_components/MainCanva.tsx
@@ -7,7 +7,7 @@ import { Module } from "@prisma/client";
 export const MainCanva = ({ modules }: { modules: Module[] }) => {
     const { labelModule } = useModuleStore();
     const currentModule = modules?.find(m => m.label === labelModule);
-    if (!currentModule?.customUrl) return 'Cargando...'
+    // if (!currentModule?.customUrl) return 'Cargando...'
 
     return <IframeRenderer url={currentModule?.customUrl ?? ''} />;
 }
